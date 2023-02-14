0f881594cfdea81b4e8905102f17e146fc9efe31
NguyenTienHCL <101309897+NguyenTienHCL@users.noreply.github.com>
2023-02-14 12:14:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,7 @@ const nameGirl = '';
 const giftUrl = 'http://nodemy.vn';
 const eventName = "Happy Valentine's Day ❤";
 const titleCard = 'Message';
-const contentCard = 'Haluuu Sóc... Thậc ra thì... anh cũm muốn nói lâu òy nhưng mà do hnay Valentine nên anh muốn tạo 1 tí xíuuu pấc ngờ cho Sóc ihihi :3 SÓC LÀM NY ANH NHẺ ❤';
+const contentCard = 'Haluuu Sóc ngok ❤ Thậc ra thì... anh cũm muốn nói lâu òy nhưng mà do hnay Valentine nên anh muốn tạo 1 tí xíuuu pấc ngờ cho Sóc ihihi :3        SÓC LÀM NY ANH NHẺ ❤';
 const giftImage = 'Squirrel.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
-
